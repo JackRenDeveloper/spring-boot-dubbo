8f274d5f9599f23a88e19e5dc01793a46d432deb
JackRen <jackren_developer@163.com>
2019-07-12 21:27:16 +0800

    JackRenDeveloper

---
+++ consumer-user/src/main/java/com/jackren/consumer/service/UserService.java
@@ -0,0 +1,24 @@
+package com.jackren.consumer.service;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.jackren.provider.service.TicketService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Janus on 2018/7/11.
+ */
+@Service//这里是Spring的注解
+public class UserService {
+
+    // 远程引用，按照全类名从注册中心寻找
+    @Reference
+    TicketService ticketService;
+
+    public void hello(){
+        String ticket = ticketService.getTicket();
+        System.out.println("买到票了："+ticket);
+    }
+
+
+}
+
